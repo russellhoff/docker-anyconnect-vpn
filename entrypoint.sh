060676a485b70f08faafe667d2ee10151cbafc58
Jon I <captain06@gmail.com>
2019-05-02 15:03:38 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-( echo yes; echo $ANYCONNECT_PASSWORD ) | openconnect $ANYCONNECT_SERVER --user=$ANYCONNECT_USER --timestamp
+( echo yes; echo $ANYCONNECT_PASSWORD ) | openconnect $ANYCONNECT_SERVER --user=$ANYCONNECT_USER --timestamp --servercert sha256:ae0fd69f1e032481bc1aea3c0fd2ddd9a148a18a8bb34f79aed1c175cc457b94
